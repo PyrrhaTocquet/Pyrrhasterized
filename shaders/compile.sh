90af6f1e12e5e5ebb3f87db0daf75dc5da18678e
PyrrhaT <130511100+PyrrhaTocquet@users.noreply.github.com>
2025-07-20 19:29:08 +0200

    mesh shaders compile with glsl

--- shaders/compile.sh
+++ shaders/compile.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+rm *.spv
+rm meshCSM.mesh
+../slang/bin/slangc.exe CSM.mesh.slang -profile glsl_460 -o meshCSM.mesh -target glsl -entry meshMain -g
 #glslc fragmentTextureCSM.frag -o fragmentTextureCSM.spv -g
 #glslc fragmentCSM.frag -o fragmentCSM.spv -g
 #glslc fragmentPBR.frag -o fragmentPBR.spv -g
-#glslc --target-spv=spv1.5 meshPBR.mesh -o meshPBR.spv -g
+glslc --target-spv=spv1.5 legacy/meshPBR.mesh -o meshPBR.spv -g
 #glslc --target-spv=spv1.5 taskShell.task -o taskShell.spv -g
-#glslc --target-spv=spv1.5 CSM.mesh -o meshCSM.spv -g
+glslc --target-spv=spv1.5 meshCSM.mesh -o meshCSM.spv -g
 #glslc --target-spv=spv1.5 taskShadow.task -o taskShadow.spv -g
-../slang/bin/slangc.exe CSM.mesh.slang -profile spirv_1_4 -o meshCSM.spv -entry meshMain
-../slang/bin/slangc.exe CSM.amp.slang -profile spirv_1_4 -o ampCSM.spv -entry amplificationMain
-../slang/bin/slangc.exe mainPBRMaterial.mesh.slang -profile spirv_1_4 -o meshPBR.spv -entry meshMain
-../slang/bin/slangc.exe mainPBRMaterial.frag.slang -profile spirv_1_4 -o fragmentPBR.spv -entry fragMain
-../slang/bin/slangc.exe mainPBRMaterial.amp.slang -profile spirv_1_4 -o amplificationPBR.spv -entry amplificationMain
-../slang/bin/slangc.exe DepthOnlyPass.frag.slang -profile spirv_1_4 -o fragDepthOnly.spv -entry fragMain
+
+../slang/bin/slangc.exe CSM.mesh.slang -profile spirv_1_5 -o meshCSM.spv -entry meshMain -g -target spirv #not compiling somehow ????
+../slang/bin/slangc.exe mainPBRMaterial.mesh.slang -profile spirv_1_5 -o meshPBR.spv -entry meshMain -g -target spirv #not compiling somehow ????
+
+
+
+../slang/bin/slangc.exe CSM.amp.slang -profile spirv_1_4 -o ampCSM.spv -entry amplificationMain -g
+../slang/bin/slangc.exe mainPBRMaterial.frag.slang -profile spirv_1_4 -o fragmentPBR.spv -entry fragMain -g
+../slang/bin/slangc.exe mainPBRMaterial.amp.slang -profile spirv_1_4 -o amplificationPBR.spv -entry amplificationMain -g
+../slang/bin/slangc.exe DepthOnlyPass.frag.slang -profile spirv_1_4 -o fragDepthOnly.spv -entry fragMain -g
 echo "done"
 read 
